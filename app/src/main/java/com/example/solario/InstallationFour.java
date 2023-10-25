8c724ed7db685ecbba557efe0005cbee3bceb58b
Chelseazhouu <90607881+Chelseazhouu@users.noreply.github.com>
2023-10-26 10:15:21 +1100

    Questionnaire Section 2

---
+++ app/src/main/java/com/example/solario/InstallationFour.java
@@ -0,0 +1,18 @@
+package com.example.solario;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.slider.Slider;
+
+public class InstallationFour extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.installation_four);
+
+    }
+}
---
+++ app/src/main/java/com/example/solario/InstallationOne.java
@@ -0,0 +1,13 @@
+package com.example.solario;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InstallationOne extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.installation_one);
+    }
+}
